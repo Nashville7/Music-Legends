68df9a6baff75714c35fabb226c0d2dbcc60349f
Nashville James <Nashvillejames7@gmail.com>
2020-07-03 11:36:11 +0200

    new navbar + busy with responsiveness

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 function openNav() {
     document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("main").style.marginLeft = "50px";
   }
   
   /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
